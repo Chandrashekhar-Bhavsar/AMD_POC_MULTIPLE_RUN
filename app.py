17c825e066d6350913701fa83636cb8abcb4a7ee
Chandrashekhar Bhavsar <118383896+Chandrashekhar-Bhavsar@users.noreply.github.com>
2023-10-13 12:25:40 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, render_template, request, jsonify, send_from_directory
+from hdfs import InsecureClient
 import os
 import zipfile
-from hdfs import InsecureClient
 
 app = Flask(__name__)
 
@@ -17,6 +17,9 @@ if not os.path.exists(TARGET_FOLDER):
     os.makedirs(TARGET_FOLDER)
 
 app.config['UPLOAD_FOLDER'] = TARGET_FOLDER
+
+# Initialize the HDFS client
+hdfs_client = InsecureClient(hdfs_host, user=hdfs_user)
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -48,14 +51,43 @@ def upload_file():
         return jsonify({'message': f'File successfully uploaded, extracted, and moved to HDFS: {folder_name}'}), 200
 
 def move_to_hdfs(local_path, folder_name):
-    # Initialize an HDFS client
-    client = InsecureClient(hdfs_host, user=hdfs_user)
+    try:
+        # Copy the local folder to HDFS
+        hdfs_dest_path = f'/Benchmark_Artifacts/{folder_name}'  # Specify the destination path in HDFS
 
-    # Copy the local folder to HDFS
-    hdfs_dest_path = f'/Benchmark_Artifacts/{folder_name}'  # Specify the destination path in HDFS
+        hdfs_client.upload(hdfs_dest_path, local_path, n_threads=5)
+        print(f"{local_path} copied to HDFS at {hdfs_dest_path}")
+    except Exception as e:
+        print(f"An error occurred while moving to HDFS: {e}")
 
-    client.upload(hdfs_dest_path, local_path, n_threads=5)
-    print(f"{local_path} copied to HDFS at {hdfs_dest_path}")
+@app.route('/list-files')
+def list_files():
+    # Define the HDFS directory path
+    hdfs_directory = '/Benchmark_Artifacts'
+
+    try:
+        # List the files in the HDFS directory
+        files = hdfs_client.list(hdfs_directory)
+        print(files)
+        return render_template('files.html', files=files)
+    except Exception as e:
+        return render_template('error.html', error=str(e))
+    
+@app.route('/list-folder-contents', methods=['POST'])
+def list_folder_contents():
+    try:
+        data = request.get_json()
+        folder_name = data['folder_name']
+
+        # Define the HDFS directory path
+        hdfs_directory = f'/Benchmark_Artifacts/{folder_name}'
+
+        # List the files in the HDFS directory
+        files = hdfs_client.list(hdfs_directory)
+        return jsonify({'contents': files})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
 
 @app.route('/')
 def index():
